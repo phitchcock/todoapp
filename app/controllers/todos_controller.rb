cb4c769b098495790d561d2437be57d67ca07c50
phitchcock <phitchcock@me.com>
2015-06-02 11:18:47 -0700

    temp removing before_action :authenticate_user!"

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show]
+  #before_action :authenticate_user!, except: [:index, :show]
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
   def index
